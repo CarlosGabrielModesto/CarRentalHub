4eb0c2119354b871fe280db8c4e7ab39cc6c063e
Carlos Gabriel <gabriel.s.modesto2@gmail.com>
2023-05-30 22:53:48 -0300

    Add files via upload

---
+++ CarRentalHub/patioautomoveis-saep/conecta.php
@@ -0,0 +1,22 @@
+<?php
+
+//Define o padrao de caracteres das páginas.
+header("content-type: text/html; chartset=utf-8");
+
+//Variáveis conexão com banco de dados.
+$host = "localhost";
+$user = "root";
+$password = "root";
+$database = "patioautomoveis_db";
+
+//Comando de conexão com o banco de dados MySQL.
+$conectaBD = mysqli_connect($host, $user, $password, $database);
+
+  //Retorna o código do erro de conexão com a base de dados.
+  if (!$conectaBD) {
+    print("Falha na conexão com a base de dados... Código do erro: " . mysqli_connect_errno());
+  }
+
+  //Define o padrao de caracteres do banco de dados.
+  mysqli_set_charset($conectaBD, "utf8");
+?>
